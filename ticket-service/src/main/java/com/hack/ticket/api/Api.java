6a16e313cd29cb80cc3b0b307986e19e6997964e
erchiggins <emilyrchiggins@gmail.com>
2017-11-10 15:01:47 -0500

    more tickets

--- ticket-service/src/main/java/com/hack/ticket/api/Api.java
+++ ticket-service/src/main/java/com/hack/ticket/api/Api.java
@@ -3,7 +3,9 @@ package com.hack.ticket.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hack.ticket.dao.TicketDao;
@@ -15,7 +17,7 @@ public class Api {
 	@Autowired
 	TicketDao ticketDao;
 	
-	@RequestMapping(value="/ticket/test")
+	@RequestMapping(value="/ticket/test", method=RequestMethod.GET)
 	public String testThings(){
 		return "Eureka!";
 	}
@@ -24,5 +26,16 @@ public class Api {
 	public Iterable<Ticket> getAllTickets(){
 		return ticketDao.findAll();
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public boolean createTicket(@RequestBody Ticket ticket){
+		if (ticketDao.save(ticket) != null){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
 
 }
