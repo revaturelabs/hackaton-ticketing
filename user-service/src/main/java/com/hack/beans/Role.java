f4454c667f8502d237656d9f100b3732ce686112
Genesis Bonds <genesis.bonds@revature.com>
2017-11-10 11:47:41 -0500

    committing bean and pom changes

--- user-service/src/main/java/com/hack/beans/Role.java
+++ user-service/src/main/java/com/hack/beans/Role.java
@@ -15,5 +15,31 @@ public class Role {
 	
 	@Column(nullable=false)
 	String name;
+	
+	public Role(){}
 
+	public Role(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
 }
--- user-service/src/main/java/com/hack/beans/User.java
+++ user-service/src/main/java/com/hack/beans/User.java
@@ -36,6 +36,68 @@ public class User {
 	
 	@Column(nullable=false)
 	private String password;
+
+	public User(){}
+	
+	public User(int id, String firstname, String lastname, Role role, String email, String password) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.role = role;
+		this.email = email;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 	
 	
 	
