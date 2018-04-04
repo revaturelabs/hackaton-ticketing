b4ef478b15b8727d78c8b229241ec94ab0e150bb
erchiggins <emilyrchiggins@gmail.com>
2018-04-04 19:04:41 -0400

    resolved dependency issues, finally

--- user-service/src/main/java/com/hack/user/api/Api.java
+++ user-service/src/main/java/com/hack/user/api/Api.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Api {
 	
-	@RequestMapping(value="/user/test")
+	@RequestMapping(value="/test")
 	public String testThings(){
 		return "Eureka!";
 	}
